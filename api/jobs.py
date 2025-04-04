6232dd43fccd0c8f849d36dfc4e0d2ea4525e3b5
vovan06 <barashewova@gmail.com>
2025-04-04 16:30:36 +0300

    lesson 04.04.25

--- api/jobs.py
+++ api/jobs.py
@@ -32,3 +32,22 @@ def get_jobs_by_id(job_id):
 @blueprint.errorhandler(404)
 def not_found():
     return flask.make_response(flask.jsonyfy({"error": "Not Found"}))
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def add_job():
+    if not flask.request.json:
+        return flask.make_response(flask.jsonyfy({"error": "Error adding job"}), 400)
+    if not all(key in ["job","work_size","colobaratoras","leader.name", "is_finished"] for key in flask.request.json.keys()):
+        return flask.make_response(flask.jsonyfy({"error": "Error adding job"}), 400)
+    job = Jobs()
+    job.job = flask.request.json["job"]
+    job.work_size = int(flask.request.json["work_size"])
+    job.collaborators = flask.request.json["collaborators"]
+    job.is_finished = flask.request.json["is_finished"]
+    job.team_leader = flask.request.json["team_leader"]
+
+    sess = db_session.create_session()
+    sess.add(job)
+    sess.commit()
+
+    return flask.make_response(flask.jsonyfy({"ok": str(job.id)}), 201)
